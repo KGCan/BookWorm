57663403e92c7342650172ff1576b936884fd06e
PacDraft <Kirsten@PacificDraftWorks.com>
2022-03-22 11:03:21 -0700

    added no changes comments to index.css and index.js, updated code in app.js to use graphql

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import{
+  ApolloClient, 
+  MemoryCache, 
+  ApolloProvider, 
+  urlLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client.link.context';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// add in GraphQL endpoing
+const urlLink = createUrlLink({
+  uri: '/graphql',
+});
+
+const auth = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(urlLink),
+  cache: new MemoryCache(),
+});
+
 function App() {
   return (
+    // add Apollo
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +47,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,5 @@
+// no changes to starter code
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
